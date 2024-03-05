bf6dbabd4bb63fae1df4dcd6c1088c4491ea37f0
VladKojima <102826875+VladKojima@users.noreply.github.com>
2024-03-05 21:44:50 +0400

    init

---
+++ backend/buildingReports/src/main/java/ru/duvalov/buildingReports/BuildingReportsApplication.java
@@ -0,0 +1,12 @@
+package ru.duvalov.buildingReports;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildingReportsApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(BuildingReportsApplication.class, args);
+	}
+
+}
