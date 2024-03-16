dd50bc5ef041f646669e62225429691008e8011d
VladKojima <102826875+VladKojima@users.noreply.github.com>
2024-03-16 23:16:30 +0400

    front: API changed

--- frontend/src/API/buildingsAPI.js
+++ frontend/src/API/buildingsAPI.js
@@ -1,10 +1,10 @@
 const urlBase = '/api'
 
-export async function getBuildingById(id) {
-    if (isNaN(id))
-        throw Error('id is NaN');
+export async function getBuildingWithStarts(str) {
+    if (typeof(str) !== 'string')
+        throw Error('str isn\'t string');
 
-    return fetch(`${urlBase}/buildings/${id}`);
+    return fetch(`${urlBase}/buildings/${str}`).then(res=>res.json());
 }
 
 export async function getBuildingList(limit, skip) {
@@ -27,14 +27,14 @@ export async function getBuildingList(limit, skip) {
     if (params.length !== 0)
         params = `?${params.join('&')}`;
 
-    return fetch(`${urlBase}/buildings/list` + params);
+    return fetch(`${urlBase}/buildings/list` + params).then(res=>res.json());
 }
 
 export async function getTicketById(id) {
     if (isNaN(id))
         throw Error('id is NaN');
 
-    return fetch(`${urlBase}/tickets/${id}`);
+    return fetch(`${urlBase}/tickets/${id}`).then(res=>res.json());
 }
 
 export async function getTicketList(limit, skip) {
@@ -57,5 +57,18 @@ export async function getTicketList(limit, skip) {
     if (params.length !== 0)
         params = `?${params.join('&')}`;
 
-    return fetch(`${urlBase}/tickets/list` + params);
+    return fetch(`${urlBase}/tickets/list` + params).then(res=>res.json());
+}
+
+export async function addTicket(ticket){
+
+    return fetch(`${urlBase}/tickets/add`, {method: "post", headers:{"Content-Type":"application/json"}, body: JSON.stringify(ticket)});
+}
+
+export async function resolveTicket(id){
+
+}
+
+export async function rejectTicket(id){
+    
 }
