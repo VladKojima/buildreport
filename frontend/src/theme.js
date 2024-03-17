aa68930b5d371f4582e31be9518328f1aa9a6963
VladKojima <102826875+VladKojima@users.noreply.github.com>
2024-03-17 22:12:11 +0400

    front: set theme

---
+++ frontend/src/theme.js
@@ -0,0 +1,9 @@
+import {createTheme} from '@mui/material/styles';
+import { deepOrange, indigo } from '@mui/material/colors';
+
+export default createTheme({
+    palette:{
+        primary: deepOrange,
+        secondary: indigo
+    }
+})
