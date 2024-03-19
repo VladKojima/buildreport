21f457f8b3a86b5697d197b312841fee3917cd96
VladKojima <102826875+VladKojima@users.noreply.github.com>
2024-03-19 17:43:24 +0400

    back: sec env

--- backend/buildingReports/src/main/java/ru/duvalov/buildingReports/security/JwtUtils.java
+++ backend/buildingReports/src/main/java/ru/duvalov/buildingReports/security/JwtUtils.java
@@ -6,7 +6,6 @@ import java.util.Date;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
 
 import io.jsonwebtoken.*;
@@ -14,32 +13,33 @@ import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 
 @Component
+@SuppressWarnings("deprecation")
 public class JwtUtils {
   private static final Logger logger = LoggerFactory.getLogger(JwtUtils.class);
 
   @Value("${duvalov.app.jwtSecret}")
   private String jwtSecret;
 
-  @Value("${duvalov.app.jwtExpirationMs}")
-  private int jwtExpirationMs;
+  @Value("${duvalov.app.jwtExpiration}")
+  private int jwtExpiration;
 
   public String generateJwtToken(String username) {
 
     return Jwts.builder()
         .setSubject(username)
         .setIssuedAt(new Date())
-        .setExpiration(new Date((new Date()).getTime() + jwtExpirationMs))
+        .setExpiration(new Date((new Date()).getTime() + jwtExpiration * 1000))
         .signWith(key(), SignatureAlgorithm.HS256)
         .compact();
   }
-  
+
   private Key key() {
     return Keys.hmacShaKeyFor(Decoders.BASE64.decode(jwtSecret));
   }
 
   public String getUserNameFromJwtToken(String token) {
     return Jwts.parser().setSigningKey(key()).build()
-               .parseClaimsJws(token).getBody().getSubject();
+        .parseClaimsJws(token).getBody().getSubject();
   }
 
   public boolean validateJwtToken(String authToken) {
