b4feb21767a5764c9afd38116061db1288ed3b18
VladKojima <102826875+VladKojima@users.noreply.github.com>
2024-03-20 00:45:22 +0400

    ticket url

--- frontend/src/Pages/Ticket.jsx
+++ frontend/src/Pages/Ticket.jsx
@@ -5,6 +5,10 @@ import Stack from '@mui/material/Stack'
 import Typography from '@mui/material/Typography';
 import AutoComplete from '@mui/material/Autocomplete';
 import SnackBar from '@mui/material/Snackbar';
+import Dialog from '@mui/material/Dialog';
+import DialogTitle from '@mui/material/DialogTitle';
+import DialogContent from '@mui/material/DialogContent';
+import Link from '@mui/material/Link';
 
 import { useRef, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
@@ -22,6 +26,8 @@ export default function Ticket() {
 
     const [snackOpen, setSnackOpen] = useState(false);
     const [snackCode, setSnackCode] = useState();
+
+    const [id, setId] = useState();
 
     function handleClose(_, reason) {
         if (reason === 'clickaway') {
@@ -56,7 +62,7 @@ export default function Ticket() {
         e.preventDefault();
         try {
             let res = await addTicket({ ...Object.fromEntries(new FormData(e.target)), buildingId: selectedId });
-            nav(`/ticket/${res.id}`);
+            setId(res.id);
         }
         catch (err) {
             setSnackCode(err.response?.status);
@@ -112,6 +118,28 @@ export default function Ticket() {
                 >Submit</Button>
             </Stack>
         </form>
-    </Container>
+        <Dialog
+            open={id != null}
+        >
+            <DialogTitle>Ticket is success added</DialogTitle>
+            <DialogContent>
+
+                <Typography>
+                    Your ticket can be accessed by link: <Link
+                        sx={{cursor: 'pointer'}}
+                        onClick={({ target }) => {
+                            navigator.clipboard.writeText(target.textContent);
+                        }}
+                    >{`${process.env.REACT_APP_DOMAIN_NAME}/ticket/${id}`}
+                    </Link> (click to copy to clipboard)
+                </Typography>
+
+                <Button
+                    onClick={() => nav("/")}
+                    variant='contained'
+                >OK</Button>
+            </DialogContent>
+        </Dialog>
+    </Container >
 
 }
