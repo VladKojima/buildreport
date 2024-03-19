46a3c827036d678c7f506647d68ca3e73df11a3b
VladKojima <102826875+VladKojima@users.noreply.github.com>
2024-03-19 22:22:26 +0400

    front: ticketinfo

--- frontend/src/Pages/TicketInfo.jsx
+++ frontend/src/Pages/TicketInfo.jsx
@@ -1,23 +1,58 @@
 import Container from '@mui/material/Container';
-import Typography from '@mui/material/Container';
+import Alert from '@mui/material/Alert';
+import AlertTitle from '@mui/material/AlertTitle';
+import CircularProgress from '@mui/material/CircularProgress';
+import Typography from '@mui/material/Typography';
+import CheckIcon from '@mui/icons-material/Check';
+import RemoveIcon from '@mui/icons-material/Remove'
 
-import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 import { getTicketById } from '../API/buildingsAPI';
+import useLoading from '../Hooks/useLoading';
+import getAlert from '../Utils/errorAlerts';
 
 export default function TicketInfo() {
     const params = useParams();
 
-    const [ticket, setTicket] = useState();
+    const { isLoading, res: ticket, error } = useLoading(() => getTicketById(params["id"]));
 
-    useEffect(() => {
-        getTicketById(params['id']).then(res => setTicket(res));
-    }, []);
+    const statusViews = {
+        "OPEN": {
+
+        },
+
+        "RESOLVED": {
+            icon: <CheckIcon />,
+            color: "success"
+        },
+
+        "REFUSED": {
+            icon: <RemoveIcon />,
+            color: "error"
+        }
+    }
 
     return <Container sx={{ marginTop: '3%' }}>
-        {ticket && <Typography>
-            Your ticket #{ticket.id}: "{ticket.title}" is {ticket.status}
-        </Typography>}
+        {
+            isLoading && <>
+                <CircularProgress color='primary' />
+                <Typography severity='info'>Loading...</Typography>
+            </>
+        }
+
+        {!isLoading && <>
+            {
+                error
+                    ? <Alert severity='error'>
+                        <AlertTitle>Error</AlertTitle>
+                        {getAlert(error.response?.status)}
+                    </Alert>
+                    : <Alert severity='info' {...statusViews[ticket.status]}>
+                        Ticket #{ticket.id}: "{ticket.title}" is {ticket.status}
+                    </Alert>
+            }
+
+        </>}
     </Container>
 }
